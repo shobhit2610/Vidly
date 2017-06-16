7d3d7e0ab399250f7961726a941462326f8601f1
shobhit2610 <mailsbhatnagar@gmail.com>
2017-06-16 23:04:01 +0530

    Data Migration Basics

--- Vidly/Vidly/Models/IdentityModels.cs
+++ Vidly/Vidly/Models/IdentityModels.cs
@@ -20,9 +20,11 @@ namespace Vidly.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Customer> Customers { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
+
         }
 
         public static ApplicationDbContext Create()
